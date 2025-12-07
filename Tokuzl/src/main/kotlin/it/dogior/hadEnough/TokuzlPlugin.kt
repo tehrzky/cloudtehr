bb9304e2568c53509404946567119b70ef05457b
tehrzky <61300808+tehrzky@users.noreply.github.com>
2025-12-07 18:26:36 +0800

    Create TokuzlPlugin.kt

---
+++ Tokuzl/src/main/kotlin/it/dogior/hadEnough/TokuzlPlugin.kt
@@ -0,0 +1,13 @@
+package it.dogior.hadEnough
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class TokuzlPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(TokuzlProvider())
+        registerExtractorAPI(P2PPlayExtractor())
+    }
+}
