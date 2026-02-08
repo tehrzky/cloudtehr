f908dbc3cef99d40df279b4a228ad682bbece8fd
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-02-08 15:42:22 +0800

    Update MovHubPlugin.kt

--- MovHub/src/main/kotlin/it/dogior/hadEnough/MovHubPlugin.kt
+++ MovHub/src/main/kotlin/it/dogior/hadEnough/MovHubPlugin.kt
@@ -6,11 +6,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class MovHubPlugin : Plugin() {
-    override val name = "MovHub"
-    override val author = "YourName"
-    override val description = "Watch movies and TV shows from MovHub"
-    override val version = "1.0.0"
-    
     override fun load(context: Context) {
         // Register the main API with default settings
         registerMainAPI(MovHub("en", true))
