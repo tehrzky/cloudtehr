a8abfada55b1863b0bf91f2ba247299c475c8aa2
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-02-08 15:47:27 +0800

    Update MovHubPlugin.kt

--- MovHub/src/main/kotlin/it/dogior/hadEnough/MovHubPlugin.kt
+++ MovHub/src/main/kotlin/it/dogior/hadEnough/MovHubPlugin.kt
@@ -7,10 +7,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class MovHubPlugin : Plugin() {
     override fun load(context: Context) {
-        // Register the main API with default settings
-        registerMainAPI(MovHub("en", true))
-        
-        // Register the extractor for video links
+        registerMainAPI(MovHub())
         registerExtractorAPI(RapidShareExtractor())
     }
 }
