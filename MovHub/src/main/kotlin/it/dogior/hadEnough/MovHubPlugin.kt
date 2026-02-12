69178c97c6201bd0e3a6361a9bc43feddaa959da
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-02-12 09:46:53 +0800

    Update MovHubPlugin.kt

--- MovHub/src/main/kotlin/it/dogior/hadEnough/MovHubPlugin.kt
+++ MovHub/src/main/kotlin/it/dogior/hadEnough/MovHubPlugin.kt
@@ -1,13 +1,17 @@
 package it.dogior.hadEnough
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.extractorApis
 
 @CloudstreamPlugin
 class MovHubPlugin : Plugin() {
     override fun load(context: Context) {
+        // Register the main provider
         registerMainAPI(MovHub())
+        
+        // Register the extractor
         registerExtractorAPI(RapidShareExtractor())
     }
 }
